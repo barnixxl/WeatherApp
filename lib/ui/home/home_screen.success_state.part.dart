fd7696d51733c4dbaa404cc67db85906b0b8594e
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-19 12:06:22 +0300

    remove if and use ListView

--- lib/ui/home/home_screen.success_state.part.dart
+++ lib/ui/home/home_screen.success_state.part.dart
@@ -5,26 +5,20 @@ Widget _buildSuccessWidget({
   required double latitude,
   required double longitude,
 }) {
-  return ListView.builder(
+  return ListView(
     padding: const EdgeInsets.all(
       8,
     ),
-    itemCount: dayWeather.length + 1,
-    itemBuilder: (
-      context,
-      index,
-    ) {
-      if (index < dayWeather.length) {
-        final item = dayWeather[index];
-        return DayWeatherItem(
+    children: [
+      ...dayWeather.map(
+        (item) => DayWeatherItem(
           dayWeather: item,
-        );
-      } else {
-        return WeatherMapWidget(
-          latitude: latitude,
-          longitude: longitude,
-        );
-      }
-    },
+        ),
+      ),
+      WeatherMapWidget(
+        latitude: latitude,
+        longitude: longitude,
+      ),
+    ],
   );
 }
