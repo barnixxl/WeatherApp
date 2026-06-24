0c2ed83f4c66ea90076b252a48391a81727a42a6
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-24 18:12:53 +0300

    add path to error image

--- lib/resources/images/map_images/map_images.dart
+++ lib/resources/images/map_images/map_images.dart
@@ -2,4 +2,5 @@ class MapImages {
   static const _baseImagePath = 'assets/images/map_images';
   static const locationOn = '$_baseImagePath/location_on.png';
   static const locationPin = '$_baseImagePath/location_pin.png';
+  static const errorOutline = '$_baseImagePath/error_outline.png';
 }
