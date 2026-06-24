35897c341583ef29e55f58f7b916dc938b9a302e
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-24 16:42:45 +0300

    add path to map images

---
+++ lib/resources/images/map_images/map_images.dart
@@ -0,0 +1,5 @@
+class MapImages {
+  static const _baseImagePath = 'assets/images';
+  static const usd = '$_baseImagePath/location_on.png';
+  static const eur = '$_baseImagePath/location_pin.png';
+}
