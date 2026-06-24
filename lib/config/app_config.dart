c535b0567a61b05a5de56b0a1af5335a8f138826
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-24 16:36:15 +0300

    add path of map to env file and use constants in the map builder

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,5 +2,8 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   static String get apiKey => dotenv.env['WEATHER_API_KEY'] ?? '';
+
+  static const mapTileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+  static const mapUserAgent = 'com.example.weather_app';
 }
 
--- lib/ui/home/widgets/weather_map_widget.dart
+++ lib/ui/home/widgets/weather_map_widget.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
+import '../../../config/app_config.dart';
 import '../../../main.dart';
 import '../../../resources/colors/app_colors.dart';
 
@@ -86,9 +87,8 @@ class WeatherMapWidget extends StatelessWidget {
                 ),
                 children: [
                   TileLayer(
-                    urlTemplate:
-                        'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
-                    userAgentPackageName: 'com.example.weather_app',
+                    urlTemplate: AppConfig.mapTileUrl,
+                    userAgentPackageName: AppConfig.mapUserAgent,
                   ),
                   MarkerLayer(
                     markers: [
