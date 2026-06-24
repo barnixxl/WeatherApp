fc6558ddfa55385e343a0ef85cb6692f17aed812
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-24 16:31:40 +0300

    use observable to map

--- lib/ui/home/home_screen.dart
+++ lib/ui/home/home_screen.dart
@@ -52,43 +52,58 @@ class _HomeScreenState extends State<HomeScreen> {
           },
         ),
       ),
-      body: RefreshIndicator(
-        onRefresh: _onRefreshPressed,
-        child: Stack(
-          children: [
-            Observer(
-              builder: (_) {
-                return Visibility(
-                  visible: homeController.isLoading,
-                  child: _buildLoadingWidget(),
-                );
-              },
-            ),
-            Observer(
-              builder: (_) {
-                return Visibility(
-                  visible: homeController.hasError,
-                  child: _buildErrorWidget(
-                    error: homeController.error,
-                    onRetryPressed: _onRefreshPressed,
+      body: Column(
+        children: [
+          Expanded(
+            child: RefreshIndicator(
+              onRefresh: _onRefreshPressed,
+              child: Stack(
+                children: [
+                  Observer(
+                    builder: (_) {
+                      return Visibility(
+                        visible: homeController.isLoading,
+                        child: _buildLoadingWidget(),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
-            Observer(
-              builder: (_) {
-                return Visibility(
-                  visible: homeController.hasSuccess,
-                  child: _buildSuccessWidget(
-                    dayWeather: homeController.dayWeather,
-                    latitude: homeController.latitude,
-                    longitude: homeController.longitude,
+                  Observer(
+                    builder: (_) {
+                      return Visibility(
+                        visible: homeController.hasError,
+                        child: _buildErrorWidget(
+                          error: homeController.error,
+                          onRetryPressed: _onRefreshPressed,
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
+                  Observer(
+                    builder: (_) {
+                      return Visibility(
+                        visible: homeController.hasSuccess,
+                        child: _buildSuccessWidget(
+                          dayWeather: homeController.dayWeather,
+                        ),
+                      );
+                    },
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
+          ),
+          Observer(
+            builder: (_) {
+              return Visibility(
+                visible: homeController.hasSuccess,
+                child: WeatherMapWidget(
+                  latitude: homeController.latitude,
+                  longitude: homeController.longitude,
+                ),
+              );
+            },
+          ),
+        ],
       ),
     );
   }
