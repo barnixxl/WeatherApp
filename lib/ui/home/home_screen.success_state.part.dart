f407785295e135d4f516549a6813f4f752727bc0
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-24 16:30:36 +0300

    use listView.builder

--- lib/ui/home/home_screen.success_state.part.dart
+++ lib/ui/home/home_screen.success_state.part.dart
@@ -2,26 +2,23 @@ part of 'home_screen.dart';
 
 Widget _buildSuccessWidget({
   required List<DayWeather> dayWeather,
-  required double latitude,
-  required double longitude,
 }) {
-  return ListView(
+  return ListView.builder(
     padding: const EdgeInsets.all(
       8,
     ),
-    children: [
-      ...dayWeather.map(
-        (item) => DayWeatherItem(
-              date: item.date,
-              dayMinTemp: item.dayMinTemp,
-              dayMaxTemp: item.dayMaxTemp,
-              hourlyData: item.hourlyData,
-            ),
-      ),
-      WeatherMapWidget(
-        latitude: latitude,
-        longitude: longitude,
-      ),
-    ],
+    itemCount: dayWeather.length,
+    itemBuilder: (
+      context,
+      index,
+    ) {
+      final item = dayWeather[index];
+      return DayWeatherItem(
+        date: item.date,
+        dayMinTemp: item.dayMinTemp,
+        dayMaxTemp: item.dayMaxTemp,
+        hourlyData: item.hourlyData,
+      );
+    },
   );
 }
