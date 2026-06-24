e88536ae02db5982a34aa1056e51e77f13084184
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-24 19:29:43 +0300

    fix commas

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -2,7 +2,6 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class AppConfig {
   static String get apiKey => dotenv.env['WEATHER_API_KEY'] ?? '';
-
   static const mapTileUrl = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
   static const mapUserAgent = 'com.example.weather_app';
 }
