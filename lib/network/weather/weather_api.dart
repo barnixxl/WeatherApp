e59132b4d3f92e7262d777d7a37c0af9b697c11d
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-25 19:30:03 +0300

    fix parsing of data in api's file

--- lib/network/weather/weather_api.dart
+++ lib/network/weather/weather_api.dart
@@ -1,12 +1,9 @@
 import 'package:get_it/get_it.dart';
 
 import '../../config/app_config.dart';
-import '../../main.dart';
-import '../../models/weather_data.dart';
 import '../../models/weather_error.dart';
 import '../../models/weather_forecast_data.dart';
 import '../../models/weather_result.dart';
-import '../../utils/int_extensions.dart';
 import '../weather_network.dart';
 import 'resp/weather_response_from_network.dart';
 
@@ -45,52 +42,25 @@ class WeatherApi {
         queryParameters: queryParams,
       );
       if (result.error == null) {
-        return _parseForecastResponse(
-          result.data,
-        );
-      } else {
-        return WeatherResult.failure(
-          result.error,
-        );
+        final data = result.data;
+        if (data != null) {
+          final response = WeatherResponseFromNetwork.fromJson(
+            data,
+          );
+          return WeatherResult.success(
+            WeatherForecastData.fromNetworkModel(
+              response,
+            ),
+          );
+        }
       }
+      return WeatherResult.failure(
+        result.error ?? WeatherError.noData(),
+      );
     } catch (e) {
       return WeatherResult.failure(
         WeatherError.fromException(e),
       );
     }
-  }
-
-  WeatherResult<WeatherForecastData> _parseForecastResponse(
-    Map<String, dynamic>? data,
-  ) {
-    if (data != null) {
-      final response = WeatherResponseFromNetwork.fromJson(
-        data,
-      );
-      return WeatherResult.success(
-        WeatherForecastData(
-          weatherData: (response.list ?? [])
-              .map(
-                (e) => WeatherData(
-                  dateTime: e.dt.toDateTimeFromUnixSeconds() ?? DateTime.now(),
-                  temperature: e.main?.temp ?? 0.0,
-                  tempMin: e.main?.tempMin ?? 0.0,
-                  tempMax: e.main?.tempMax ?? 0.0,
-                  weatherMain: e.weather?.firstOrNull?.main ?? '',
-                  weatherDescription: e.weather?.firstOrNull?.description ?? '',
-                  weatherImageCode: e.weather?.firstOrNull?.icon ?? '',
-                  windSpeed: e.wind?.speed ?? 0.0,
-                ),
-              )
-              .toList(),
-          cityName: response.city?.name ?? strings.common_unknow_city_name,
-          latitude: response.city?.coord?.lat ?? 0.0,
-          longitude: response.city?.coord?.lon ?? 0.0,
-        ),
-      );
-    }
-    return WeatherResult.failure(
-      WeatherError.noData(),
-    );
   }
 }
