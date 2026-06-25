591ea8f9380b7988cd9da55a39cf704fa01a93ec
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-06-25 18:23:21 +0300

    use final to location states

--- lib/utils/location_service.dart
+++ lib/utils/location_service.dart
@@ -17,15 +17,13 @@ class LocationService {
   }
 
   Future<Position?> getCurrentLocation() async {
-    bool serviceEnabled;
-    LocationPermission permission;
-    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    final serviceEnabled = await Geolocator.isLocationServiceEnabled();
     if (!serviceEnabled) {
       return null;
     }
-    permission = await Geolocator.checkPermission();
+    final permission = await Geolocator.checkPermission();
     if (permission == LocationPermission.denied) {
-      permission = await Geolocator.requestPermission();
+      final permission = await Geolocator.requestPermission();
       if (permission == LocationPermission.denied) {
         return null;
       }
