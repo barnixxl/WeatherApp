dcd426bff12198afee7836235ec8183e21bd9d3e
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-01 17:22:44 +0300

    use notInitialized in controller

--- lib/ui/home/home_controller.dart
+++ lib/ui/home/home_controller.dart
@@ -10,7 +10,7 @@ class HomeController {
   final WeatherRepository _repository = WeatherRepository.getInstance();
 
   final Observable<WeatherResult<ForecastData>> _weatherResult = Observable(
-    WeatherResult.loading(),
+    WeatherResult.notInitialized(),
   );
 
   final Observable<String> _appBarCityName = Observable(
