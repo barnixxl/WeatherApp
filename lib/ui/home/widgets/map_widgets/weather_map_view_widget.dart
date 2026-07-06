0d2f36c54e19a3583da1fcd5e23de67d3e0e11e7
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-06 14:52:20 +0300

    add contributors to map widget

--- lib/ui/home/widgets/map_widgets/weather_map_view_widget.dart
+++ lib/ui/home/widgets/map_widgets/weather_map_view_widget.dart
@@ -3,6 +3,8 @@ import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
 
 import '../../../../config/app_config.dart';
+import '../../../../main.dart';
+import '../../../../resources/colors/app_colors.dart';
 import '../../../../resources/images/map_images/map_images.dart';
 
 class WeatherMapView extends StatelessWidget {
@@ -20,39 +22,53 @@ class WeatherMapView extends StatelessWidget {
     BuildContext context,
   ) {
     return RepaintBoundary(
-      child: FlutterMap(
-        key: ValueKey(
-          '$latitude,'
-          '$longitude,',
-        ),
-        options: MapOptions(
-          initialCenter: LatLng(
-            latitude,
-            longitude,
-          ),
-          initialZoom: 10.0,
-        ),
+      child: Column(
         children: [
-          TileLayer(
-            urlTemplate: AppConfig.mapTileUrl,
-            userAgentPackageName: AppConfig.mapUserAgent,
-          ),
-          MarkerLayer(
-            markers: [
-              Marker(
-                point: LatLng(
+          Expanded(
+            child: FlutterMap(
+              key: ValueKey(
+                '$latitude,'
+                '$longitude,',
+              ),
+              options: MapOptions(
+                initialCenter: LatLng(
                   latitude,
                   longitude,
                 ),
-                width: 40,
-                height: 40,
-                child: Image.asset(
-                  MapImages.locationPin,
-                  width: 40,
-                  height: 40,
-                ),
+                initialZoom: 10.0,
               ),
-            ],
+              children: [
+                TileLayer(
+                  urlTemplate: AppConfig.mapTileUrl,
+                  userAgentPackageName: AppConfig.mapUserAgent,
+                ),
+                MarkerLayer(
+                  markers: [
+                    Marker(
+                      point: LatLng(
+                        latitude,
+                        longitude,
+                      ),
+                      width: 40,
+                      height: 40,
+                      child: Image.asset(
+                        MapImages.locationPin,
+                        width: 40,
+                        height: 40,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Text(
+            strings.map_osm_attribution,
+            style: const TextStyle(
+              fontSize: 10,
+              color: AppColors.primaryGray,
+            ),
+            textAlign: TextAlign.center,
           ),
         ],
       ),
