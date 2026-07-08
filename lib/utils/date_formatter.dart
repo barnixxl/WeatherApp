984c610206840e7508bcae7110b0ef4f8efe026d
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-08 14:49:46 +0300

    unrename util

--- lib/utils/date_formatter.dart
+++ lib/utils/date_formatter.dart
@@ -1,6 +1,6 @@
 import 'package:intl/intl.dart';
 
-extension DateFormatter on DateTime? {
+extension DateUtils on DateTime? {
   static final DateFormat dayMonthYearTextDateFormat =
       DateFormat('d MMMM y', 'ru');
   static final DateFormat dayOfWeekFormat = DateFormat('EEEE', 'ru');
