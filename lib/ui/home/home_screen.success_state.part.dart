61837bfe4520eeb36f4f7b562ef0449883ec595c
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-08 14:06:43 +0300

    remove for() and use map widget to show the map

--- lib/ui/home/home_screen.success_state.part.dart
+++ lib/ui/home/home_screen.success_state.part.dart
@@ -2,25 +2,23 @@ part of 'home_screen.dart';
 
 Widget _buildSuccessWidget({
   required List<DayWeather> dayWeather,
-  required double latitude,
-  required double longitude,
 }) {
-  return ListView(
+  return ListView.builder(
     padding: const EdgeInsets.all(
       8,
     ),
-    children: [
-      for (final item in dayWeather)
-        DayWeatherItem(
-          date: item.date,
-          dayMinTemp: item.dayMinTemp,
-          dayMaxTemp: item.dayMaxTemp,
-          hourlyData: item.hourlyData,
-        ),
-      WeatherMapWidget(
-        latitude: latitude,
-        longitude: longitude,
-      ),
-    ],
+    itemCount: dayWeather.length,
+    itemBuilder: (
+      _,
+      index,
+    ) {
+      final item = dayWeather[index];
+      return DayWeatherItem(
+        date: item.date,
+        dayMinTemp: item.dayMinTemp,
+        dayMaxTemp: item.dayMaxTemp,
+        hourlyData: item.hourlyData,
+      );
+    },
   );
 }
