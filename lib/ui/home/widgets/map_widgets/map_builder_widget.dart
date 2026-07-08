a52dddf3ecdd5ff94b121065123d5969fc38e86c
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-08 15:02:55 +0300

    remove _buildHeader() in class like class method

--- lib/ui/home/widgets/map_widgets/map_builder_widget.dart
+++ lib/ui/home/widgets/map_widgets/map_builder_widget.dart
@@ -49,45 +49,45 @@ class MapWidget extends StatelessWidget {
       ),
     );
   }
-}
 
-Widget _buildHeader(
-  BuildContext context,
-  VoidCallback onClose,
-) {
-  return Container(
-    padding: const EdgeInsets.all(
-      12,
-    ),
-    color: AppColors.primaryDark,
-    child: Row(
-      children: [
-        Image.asset(
-          MapImages.locationOn,
-          width: 20,
-          height: 20,
-        ),
-        const SizedBox(
-          width: 8,
-        ),
-        Text(
-          strings.current_location,
-          style: const TextStyle(
-            color: AppColors.onPrimary,
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
+  Widget _buildHeader(
+    BuildContext context,
+    VoidCallback onClose,
+  ) {
+    return Container(
+      padding: const EdgeInsets.all(
+        12,
+      ),
+      color: AppColors.primaryDark,
+      child: Row(
+        children: [
+          Image.asset(
+            MapImages.locationOn,
+            width: 20,
+            height: 20,
           ),
-        ),
-        const Spacer(),
-        IconButton(
-          icon: Image.asset(
-            MapImages.closeIcon,
-            width: 24,
-            height: 24,
+          const SizedBox(
+            width: 8,
           ),
-          onPressed: onClose,
-        ),
-      ],
-    ),
-  );
+          Text(
+            strings.current_location,
+            style: const TextStyle(
+              color: AppColors.onPrimary,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          const Spacer(),
+          IconButton(
+            icon: Image.asset(
+              MapImages.closeIcon,
+              width: 24,
+              height: 24,
+            ),
+            onPressed: onClose,
+          ),
+        ],
+      ),
+    );
+  }
 }
