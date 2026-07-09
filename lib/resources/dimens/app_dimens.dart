8f31c3da422b342bd1f537397798ac5f422b1bf6
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-09 16:22:19 +0300

    add AppDimens class

---
+++ lib/resources/dimens/app_dimens.dart
@@ -0,0 +1,19 @@
+class AppDimens {
+  static const double fontXxs = 10;
+  static const double fontXs = 12;
+  static const double fontSm = 13;
+  static const double fontMd = 16;
+  static const double fontLg = 18;
+
+  static const double iconXs = 20;
+  static const double iconSm = 24;
+  static const double iconMd = 32;
+  static const double iconLg = 40;
+  static const double iconXl = 64;
+
+  static const double hourlyRowHeight = 100;
+  static const double hourlyItemWidth = 80;
+  static const double mapOverlayHeight = 300;
+
+  static const double radiusMd = 16;
+}
