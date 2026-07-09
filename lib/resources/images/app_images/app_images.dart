0003731261742aa4ee20ac8e4c4c6ae524369c13
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-09 18:13:45 +0300

    fix path

--- lib/resources/images/app_images/app_images.dart
+++ lib/resources/images/app_images/app_images.dart
@@ -1,5 +1,5 @@
 class AppImages {
-  static const _baseImagePath = 'assets/images/map_images';
+  static const _baseImagePath = 'assets/images/app_images';
   static const closeIcon = '$_baseImagePath/close_image.png';
   static const mapIcon = '$_baseImagePath/map_image.png';
 }
