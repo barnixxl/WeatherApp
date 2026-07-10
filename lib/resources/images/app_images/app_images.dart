a03bedb2533c3a1e74f1859e9f471f840bd452db
Birin Mikhail <167678036+barnixxl@users.noreply.github.com>
2026-07-10 16:59:51 +0300

    move all path to images in app_images

--- lib/resources/images/app_images/app_images.dart
+++ lib/resources/images/app_images/app_images.dart
@@ -2,4 +2,28 @@ class AppImages {
   static const _baseImagePath = 'assets/images/app_images';
   static const closeIcon = '$_baseImagePath/close_image.png';
   static const mapIcon = '$_baseImagePath/map_image.png';
+
+  static const locationOn = '$_baseImagePath/location_on.png';
+  static const locationPin = '$_baseImagePath/location_pin.png';
+  static const errorOutline = '$_baseImagePath/error_outline.png';
+
+  static const sun = '$_baseImagePath/sun.png';
+  static const moon = '$_baseImagePath/moon.png';
+  static const sunBehindCloud = '$_baseImagePath/sun_behind_cloud.png';
+  static const moonBehindCloud = '$_baseImagePath/moon_behind_cloud.png';
+  static const greyClouds = '$_baseImagePath/grey_clouds.png';
+  static const greyCloudsNight = '$_baseImagePath/grey_clouds_night.png';
+  static const darkClouds = '$_baseImagePath/dark_clouds.png';
+  static const darkCloudsNight = '$_baseImagePath/dark_clouds_night.png';
+  static const rain = '$_baseImagePath/rain.png';
+  static const rainNight = '$_baseImagePath/rain_night.png';
+  static const sunRain = '$_baseImagePath/sun_rain.png';
+  static const sunRainNight = '$_baseImagePath/sun_rain_night.png';
+  static const thunderstorm = '$_baseImagePath/thunderstorm.png';
+  static const thunderstormNight = '$_baseImagePath/thunderstorm_night.png';
+  static const snow = '$_baseImagePath/snow.png';
+  static const snowNight = '$_baseImagePath/snow_night.png';
+  static const fog = '$_baseImagePath/fog.png';
+  static const fogNight = '$_baseImagePath/fog_night.png';
+  static const unknown = '$_baseImagePath/unknown.png';
 }
